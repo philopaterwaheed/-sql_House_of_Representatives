e6720eff0052ba52ee2fc1a42cbc4ade987133f6
philo san <61416026+philopaterwaheed@users.noreply.github.com>
2023-12-07 12:23:12 +0200

    some fixs

--- us_state.sql
+++ us_state.sql
@@ -1,4 +1,4 @@
 CREATE TABLE us_states (
     us_state varchar(20) PRIMARY KEY,
-    region varchar(20) CHECK (region IN ('Northeast', 'Midwest', 'Southeast', 'west')))
-) 
+    region varchar(20) CHECK (region IN ('Northeast', 'Midwest', 'Southeast', 'west'))
+);

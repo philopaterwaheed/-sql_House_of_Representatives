372e00ba5852db588c5d574de2a4e4dad6645f00
philo san <61416026+philopaterwaheed@users.noreply.github.com>
2023-12-07 12:01:36 +0200

    Minor changes to accommodate congressmen data correctly.

--- congress_persons.sql
+++ congress_persons.sql
@@ -1,8 +1,8 @@
 create Table congress_persons
-(id number(3) NOT NULL Primary key,
-	full_name char(20) Not NULL,
-    district_represented char(20) Not Null,
+(office_room varchar(10) NOT NULL Primary key UNIQUE,
+	name char(20) NOT NULL,
+    district_represented char(5) NOT Null,
     start_date DATE DEFAULT SYSDATE NOT NULL ,
-    political_party char(20) CHECK (political_party IN ('Democratic', 'Republican', 'Libertarian','other')),
+    political_party varchar(1) CHECK (political_party IN ('D', 'R', 'L', 'O')),
     state_represented char(20) NOT NULL
 )

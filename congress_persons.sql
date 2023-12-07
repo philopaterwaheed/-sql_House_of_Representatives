096d5b5b75a3ed570a5ffe4bf11542442e108b05
philo san <61416026+philopaterwaheed@users.noreply.github.com>
2023-12-07 14:17:30 +0200

    Update congress_persons.sql

--- congress_persons.sql
+++ congress_persons.sql
@@ -1,8 +1,9 @@
-create Table congress_persons
-(office_room varchar(10) NOT NULL Primary key UNIQUE,
+CREATE TABLE congress_persons (
+    office_room varchar(10) NOT NULL Primary key,
 	name char(20) NOT NULL,
     district_represented char(5) NOT Null,
-    start_date DATE DEFAULT SYSDATE NOT NULL ,
+    start_date DATE DEFAULT SYSDATE NOT NULL,
     political_party varchar(1) CHECK (political_party IN ('D', 'R', 'L', 'O')),
-    state_represented char(20) NOT NULL
-)
+    state_represented varchar(20),
+    FOREIGN KEY (state_represented) REFERENCES us_states(us_state)
+);

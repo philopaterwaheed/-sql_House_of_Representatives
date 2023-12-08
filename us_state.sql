03242295955b1d21e3616d8a97c6e0c3df1b303d
Moe <71234244+moe-the-techie@users.noreply.github.com>
2023-12-08 17:59:20 +0200

    Finalizing the design of us_state.sql
    # Modified:
    - The domain for region.
    - Length of us_state.
    
    # Added:
    - NOT NULL Constraint to region.

--- us_state.sql
+++ us_state.sql
@@ -1,4 +1,4 @@
 CREATE TABLE us_states (
-    us_state varchar(20) PRIMARY KEY,
-    region varchar(20) CHECK (region IN ('Northeast', 'Midwest', 'Southeast', 'west'))
+    us_state varchar(50) PRIMARY KEY,
+    region varchar(20) CHECK (region IN (‘Northeast’, ‘Midwest’, ‘Southeast’, ‘Southwest’, ‘West’)) NOT NULL
 );

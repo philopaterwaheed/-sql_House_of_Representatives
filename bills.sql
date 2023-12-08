bb9b3c5ddb26ae120f7e7306cd04e31f73b76395
Moe <71234244+moe-the-techie@users.noreply.github.com>
2023-12-08 17:03:14 +0200

    Finalized design for bills table
    # Added:
    - Multivalued attribute cosponsers added in it's own table.
    
    # Modified:
    - Name of the file to include both bills and cosponsors tables.
    - Constraints for bills and their attributes.

--- bills.sql
+++
@@ -1,9 +0,0 @@
-create Table bills
-(
-    sponser_id number (3) not null primary key  , 
-    bill_name char (420)  not null  ,
-    vote_date DATE DEFAULT SYSDATE NOT NULL ,
-    passed char(3) CHECK (passed IN ('yes','no')) not null
-    
-    
-);

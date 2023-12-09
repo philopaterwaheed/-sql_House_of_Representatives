4d416d8bf08d5a30e774f1c94a9242868e495b46
philo san <61416026+philopaterwaheed@users.noreply.github.com>
2023-12-10 01:21:54 +0200

    Create populate_states_lower.sql

---
+++ populate_states_lower.sql
@@ -0,0 +1,55 @@
+
+insert into us_states (us_state, region) values ('Connecticut', 'Northeast');
+insert into us_states (us_state, region) values ('Maine', 'Northeast');
+insert into us_states (us_state, region) values ('Massachusetts', 'Northeast');
+insert into us_states (us_state, region) values ('New Hampshire', 'Northeast');
+insert into us_states (us_state, region) values ('New Jersey', 'Northeast');
+insert into us_states (us_state, region) values ('New York', 'Northeast');
+insert into us_states (us_state, region) values ('Pennsylvania', 'Northeast');
+insert into us_states (us_state, region) values ('Rhode Island', 'Northeast');
+insert into us_states (us_state, region) values ('Vermont', 'Northeast');
+
+insert into us_states (us_state, region) values ('Illinois', 'Midwest');
+insert into us_states (us_state, region) values ('Indiana', 'Midwest');
+insert into us_states (us_state, region) values ('Iowa', 'Midwest');
+insert into us_states (us_state, region) values ('Kansas', 'Midwest');
+insert into us_states (us_state, region) values ('Michigan', 'Midwest');
+insert into us_states (us_state, region) values ('Minnesota', 'Midwest');
+insert into us_states (us_state, region) values ('Missouri', 'Midwest');
+insert into us_states (us_state, region) values ('Nebraska', 'Midwest');
+insert into us_states (us_state, region) values ('North Dakota', 'Midwest');
+insert into us_states (us_state, region) values ('Ohio', 'Midwest');
+insert into us_states (us_state, region) values ('South Dakota', 'Midwest');
+insert into us_states (us_state, region) values ('Wisconsin', 'Midwest');
+
+insert into us_states (us_state, region) values ('Delaware', 'Southeast');
+insert into us_states (us_state, region) values ('Maryland', 'Southeast');
+insert into us_states (us_state, region) values ('Alabama', 'Southeast');
+insert into us_states (us_state, region) values ('Arkansas', 'Southeast');
+insert into us_states (us_state, region) values ('Florida', 'Southeast');
+insert into us_states (us_state, region) values ('Georgia', 'Southeast');
+insert into us_states (us_state, region) values ('Kentucky', 'Southeast');
+insert into us_states (us_state, region) values ('Louisiana', 'Southeast');
+insert into us_states (us_state, region) values ('Mississippi', 'Southeast');
+insert into us_states (us_state, region) values ('North Carolina', 'Southeast');
+insert into us_states (us_state, region) values ('South Carolina', 'Southeast');
+insert into us_states (us_state, region) values ('Tennessee', 'Southeast');
+insert into us_states (us_state, region) values ('Virginia', 'Southeast');
+insert into us_states (us_state, region) values ('West Virginia', 'Southeast');
+
+insert into us_states (us_state, region) values ('Arizona', 'Southwest');
+insert into us_states (us_state, region) values ('New Mexico', 'Southwest');
+insert into us_states (us_state, region) values ('Oklahoma', 'Southwest');
+insert into us_states (us_state, region) values ('Texas', 'Southwest');
+
+insert into us_states (us_state, region) values ('Alaska', 'West');
+insert into us_states (us_state, region) values ('California', 'West');
+insert into us_states (us_state, region) values ('Colorado', 'West');
+insert into us_states (us_state, region) values ('Hawaii', 'West');
+insert into us_states (us_state, region) values ('Idaho', 'West');
+insert into us_states (us_state, region) values ('Montana', 'West');
+insert into us_states (us_state, region) values ('Nevada', 'West');
+insert into us_states (us_state, region) values ('Oregon', 'West');
+insert into us_states (us_state, region) values ('Utah', 'West');
+insert into us_states (us_state, region) values ('Washington', 'West');
+insert into us_states (us_state, region) values ('Wyoming', 'West');

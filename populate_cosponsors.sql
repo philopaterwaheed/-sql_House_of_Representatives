df79d627b8bf1dfaf56d1ff46b19eab39b6ef29e
philo san <61416026+philopaterwaheed@users.noreply.github.com>
2023-12-10 05:10:07 +0200

    Create populate_cosponsors.sql

---
+++ populate_cosponsors.sql
@@ -0,0 +1,32 @@
+insert into cosponsors (cosponsor_number, bill_name, cosponsor_name)
+values (2406, 'HB9701', 'Rep. Louie Gohmert');
+
+insert into cosponsors (cosponsor_number, bill_name, cosponsor_name)
+values (2443, 'HB9699', 'Rep. Louie Gohmert');
+
+insert into cosponsors (cosponsor_number, bill_name, cosponsor_name)
+values (2421, 'HB9700', 'Rep. Steve Chabot');
+
+insert into cosponsors (cosponsor_number, bill_name, cosponsor_name)
+values (2465, 'HB9690', 'Rep. Andre Carson');
+
+insert into cosponsors (cosponsor_number, bill_name, cosponsor_name)
+values (2165, 'HB9692', 'Rep. Louie Gohmert');
+
+insert into cosponsors (cosponsor_number, bill_name, cosponsor_name)
+values (24311, 'HB9687', 'Sen. Peter Welch');
+
+insert into cosponsors (cosponsor_number, bill_name, cosponsor_name)
+values (24321, 'SB314', 'Sen. Ted Cruz');
+
+insert into cosponsors (cosponsor_number, bill_name, cosponsor_name)
+values (21421, 'HB310', 'Rep. Michael Cloud');
+
+insert into cosponsors (cosponsor_number, bill_name, cosponsor_name)
+values (21461, 'HB310', 'Rep. Josh Brecheen');
+
+insert into cosponsors (cosponsor_number, bill_name, cosponsor_name)
+values (20461, 'SB5312', 'Sen. Elizabeth Warren');
+
+insert into cosponsors (cosponsor_number, bill_name, cosponsor_name)
+values (204161, 'SB5311', 'Sen. Rob Portman');

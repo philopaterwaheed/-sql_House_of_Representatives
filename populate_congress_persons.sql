231a4d09cd7b74b9ef16501f31c0700f2cf80463
Moe <71234244+moe-the-techie@users.noreply.github.com>
2023-12-10 07:28:06 +0200

    Minor fixes to populate files.

--- populate_congress_persons.sql
+++ populate_congress_persons.sql
@@ -1,44 +1,44 @@
 INSERT INTO congress_persons (room_number, full_name, district, start_date, party, state_represented)
-VALUES (1415, 'Jahana Hayes', '5 th', TO_DATE('2019-3-1', 'YYYY-MM-DD'), 'D', 'Connecticut');
+VALUES (1415, 'Jahana Hayes', '5th', TO_DATE('2019-3-1', 'YYYY-MM-DD'), 'D', 'Connecticut');
 
 INSERT INTO congress_persons (room_number, full_name, district, start_date, party, state_represented)
-VALUES (2150, 'Bill Posey', '8th', TO_DATE('2019-3-1', 'YYYY-MM-DD'), 'R', 'Florida')
+VALUES (2150, 'Bill Posey', '8th', TO_DATE('2019-3-1', 'YYYY-MM-DD'), 'R', 'Florida');
 
 INSERT INTO congress_persons (room_number, full_name, district, start_date, party, state_represented)
-VALUES (2407, 'Sanford D. Bishop, Jr.', '2nd', TO_DATE('2019-3-1', 'YYYY-MM-DD'), 'D', 'Georgia')
+VALUES (2407, 'Sanford D. Bishop, Jr.', '2nd', TO_DATE('2019-3-1', 'YYYY-MM-DD'), 'D', 'Georgia');
 
 INSERT INTO congress_persons (room_number, full_name, district, start_date, party, state_represented)
-VALUES (2417, 'Austin Scott', '8th', TO_DATE('2019-3-1', 'YYYY-MM-DD'), 'R', 'Georgia')
+VALUES (2417, 'Austin Scott', '8th', TO_DATE('2019-3-1', 'YYYY-MM-DD'), 'R', 'Georgia');
 
 INSERT INTO congress_persons (room_number, full_name, district, start_date, party, state_represented)
-VALUES (1433, 'Tulsi Gabbard', '2nd', TO_DATE('2019-3-1', 'YYYY-MM-DD'), 'D', 'Hawaii')
+VALUES (1433, 'Tulsi Gabbard', '2nd', TO_DATE('2019-3-1', 'YYYY-MM-DD'), 'D', 'Hawaii');
 
 INSERT INTO congress_persons (room_number, full_name, district, start_date, party, state_represented)
-VALUES (2188, 'Bobby L. Rush', '1st', TO_DATE('2019-3-1', 'YYYY-MM-DD'), 'D', 'Illinois')
+VALUES (2188, 'Bobby L. Rush', '1st', TO_DATE('2019-3-1', 'YYYY-MM-DD'), 'D', 'Illinois');
 
 INSERT INTO congress_persons (room_number, full_name, district, start_date, party, state_represented)
-VALUES (1233, 'Adam Kinzinger', '16th', TO_DATE('2019-3-1', 'YYYY-MM-DD'), 'R', 'Illinois')
+VALUES (1233, 'Adam Kinzinger', '16th', TO_DATE('2019-3-1', 'YYYY-MM-DD'), 'R', 'Illinois');
 
 INSERT INTO congress_persons (room_number, full_name, district, start_date, party, state_represented)
-VALUES (1037, 'James Comer', '1st', TO_DATE('2015-1-6', 'YYYY-MM-DD'), 'R', 'Kentucky')
+VALUES (1037, 'James Comer', '1st', TO_DATE('2015-1-6', 'YYYY-MM-DD'), 'R', 'Kentucky');
 
 INSERT INTO congress_persons (room_number, full_name, district, start_date, party, state_represented)
-VALUES (424, 'Clay Higgins', '3rd', TO_DATE('2019-3-1', 'YYYY-MM-DD'), 'D', 'Maryland')
+VALUES (424, 'Clay Higgins', '3rd', TO_DATE('2019-3-1', 'YYYY-MM-DD'), 'D', 'Maryland');
 
 INSERT INTO congress_persons (room_number, full_name, district, start_date, party, state_represented)
-VALUES (106, 'Justin Amash', '3rd', TO_DATE('2019-3-1', 'YYYY-MM-DD'), 'R', 'Michigan')
+VALUES (106, 'Justin Amash', '3rd', TO_DATE('2019-3-1', 'YYYY-MM-DD'), 'R', 'Michigan');
 
 INSERT INTO congress_persons (room_number, full_name, district, start_date, party, state_represented)
-VALUES (315, 'Tom Emmer', '6th', TO_DATE('2019-3-1', 'YYYY-MM-DD'), 'R', 'Minnesota')
+VALUES (315, 'Tom Emmer', '6th', TO_DATE('2019-3-1', 'YYYY-MM-DD'), 'R', 'Minnesota');
 
 INSERT INTO congress_persons (room_number, full_name, district, start_date, party, state_represented)
-VALUES (2350, 'Ann Wagner', '2nd', TO_DATE('2019-3-1', 'YYYY-MM-DD'), 'R', 'Missouri')
+VALUES (2350, 'Ann Wagner', '2nd', TO_DATE('2019-3-1', 'YYYY-MM-DD'), 'R', 'Missouri');
 
 INSERT INTO congress_persons (room_number, full_name, district, start_date, party, state_represented)
-VALUES (2080, 'G. K. Butterfield', '1st', TO_DATE('2019-3-1', 'YYYY-MM-DD'), 'D', 'North Carolina')
+VALUES (2080, 'G. K. Butterfield', '1st', TO_DATE('2019-3-1', 'YYYY-MM-DD'), 'D', 'North Carolina');
 
 INSERT INTO congress_persons (room_number, full_name, district, start_date, party, state_represented)
-VALUES (2333, 'Walter B. Jones', '3rd', TO_DATE('2019-3-1', 'YYYY-MM-DD'), 'R', 'North Carolina')
+VALUES (2333, 'Walter B. Jones', '3rd', TO_DATE('2019-3-1', 'YYYY-MM-DD'), 'R', 'North Carolina');
 
 INSERT INTO congress_persons (room_number, full_name, district, start_date, party, state_represented)
-VALUES (2442, 'Bonnie Watson Coleman', '12th', TO_DATE('2019-3-1', 'YYYY-MM-DD'), 'D', 'New Jersey')
+VALUES (2442, 'Bonnie Watson Coleman', '12th', TO_DATE('2019-3-1', 'YYYY-MM-DD'), 'D', 'New Jersey');

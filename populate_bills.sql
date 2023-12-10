125be5e1c8eba838778393f59a1ebbc87e2b48fc
philo san <61416026+philopaterwaheed@users.noreply.github.com>
2023-12-10 04:08:05 +0200

    Create populate_bills.sql

---
+++ populate_bills.sql
@@ -0,0 +1,45 @@
+insert into bills (bill_name, vote_date, passed) 
+values ('HB9701', TO_DATE('2022-12-30', 'YYYY-MM-DD'), 'no');
+
+insert into bills (bill_name, vote_date, passed) 
+values ('HB9699', TO_DATE('2022-12-30', 'YYYY-MM-DD'), 'no');
+
+insert into bills (bill_name, vote_date, passed) 
+values ('HB9700', TO_DATE('2022-12-30', 'YYYY-MM-DD'), 'no');
+
+insert into bills (bill_name, vote_date, passed) 
+values ('HB9690', TO_DATE('2022-12-26', 'YYYY-MM-DD'), 'no');
+
+insert into bills (bill_name, vote_date, passed) 
+values ('HB9692', TO_DATE('2022-12-23', 'YYYY-MM-DD'), 'no');
+
+insert into bills (bill_name, vote_date, passed) 
+values ('HR1531', TO_DATE('2022-12-23', 'YYYY-MM-DD'), 'no');
+
+insert into bills (bill_name, vote_date, passed) 
+values ('HB9687', TO_DATE('2022-12-22', 'YYYY-MM-DD'), 'no');
+
+insert into bills (bill_name, vote_date, passed) 
+values ('SB314', TO_DATE('2022-12-21', 'YYYY-MM-DD'), 'yes');
+
+insert into bills (bill_name, vote_date, passed) 
+values ('HB310', TO_DATE('2022-12-21', 'YYYY-MM-DD'), 'yes');
+
+insert into bills (bill_name, vote_date, passed) 
+values ('SB5312', TO_DATE('2022-12-20', 'YYYY-MM-DD'), 'no');
+
+insert into bills (bill_name, vote_date, passed) 
+values ('SB5311', TO_DATE('2022-12-20', 'YYYY-MM-DD'), 'no');
+
+insert into bills (bill_name, vote_date, passed) 
+values ('HB9621', TO_DATE('2022-12-20', 'YYYY-MM-DD'), 'no');
+
+insert into bills (bill_name, vote_date, passed) 
+values ('SB5310', TO_DATE('2022-12-20', 'YYYY-MM-DD'), 'no');
+
+insert into bills (bill_name, vote_date, passed) 
+values ('HB9617', TO_DATE('2022-12-20', 'YYYY-MM-DD'), 'no');
+
+insert into bills (bill_name, vote_date, passed) 
+values ('HB9639', TO_DATE('2022-12-20', 'YYYY-MM-DD'), 'no');
+

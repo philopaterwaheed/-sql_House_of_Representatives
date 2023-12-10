b28925b713897084994473a3712e792d95ebe401
philo san <61416026+philopaterwaheed@users.noreply.github.com>
2023-12-10 06:58:07 +0200

    Update populate_bills.sql

--- populate_bills.sql
+++ populate_bills.sql
@@ -43,3 +43,23 @@ values ('HB9617', TO_DATE('2022-12-20', 'YYYY-MM-DD'), 'no');
 insert into bills (bill_name, vote_date, passed) 
 values ('HB9639', TO_DATE('2022-12-20', 'YYYY-MM-DD'), 'no');
 
+insert into bills (bill_name, vote_date, passed) 
+values ('HJR27', TO_DATE('2023-4-18', 'YYYY-MM-DD'), 'yes');
+
+insert into bills (bill_name, vote_date, passed) 
+values ('HB3091', TO_DATE('2023-5-18', 'YYYY-MM-DD'), 'yes');
+
+
+insert into bills (bill_name, vote_date, passed) 
+values ('HB1118', TO_DATE('2023-7-11', 'YYYY-MM-DD'), 'yes');
+
+insert into bills (bill_name, vote_date, passed) 
+values ('HB5240', TO_DATE('2023-8-21', 'YYYY-MM-DD'), 'no');
+
+
+insert into bills (bill_name, vote_date, passed) 
+values ('HB431', TO_DATE('2023-1-20', 'YYYY-MM-DD'), 'no');
+
+
+insert into bills (bill_name, vote_date, passed) 
+values ('HB38', TO_DATE('2023-1-9', 'YYYY-MM-DD'), 'no');

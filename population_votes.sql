c41f58021ae39551cd09abfd1c71ab5d20774197
Moe <71234244+moe-the-techie@users.noreply.github.com>
2023-12-10 07:42:34 +0200

    Capitalizing populate_votes.sql

--- population_votes.sql
+++ population_votes.sql
@@ -1,102 +1,102 @@
-insert into votes (room_number, bill_name)
-values (2150, 'HB680');
+INSERT INTO votes (room_number, bill_name)
+VALUES (2150, 'HB680');
 
-insert into votes (room_number, bill_name)
-values (1037, 'HB680');
+INSERT INTO votes (room_number, bill_name)
+VALUES (1037, 'HB680');
 
-insert into votes (room_number, bill_name)
-values (504, 'HB680');
+INSERT INTO votes (room_number, bill_name)
+VALUES (504, 'HB680');
 
-insert into votes (room_number, bill_name)
-values (119, 'HB680');
+INSERT INTO votes (room_number, bill_name)
+VALUES (119, 'HB680');
 
-insert into votes (room_number, bill_name)
-values (119, 'SB3168');
+INSERT INTO votes (room_number, bill_name)
+VALUES (119, 'SB3168');
 
-insert into votes (room_number, bill_name)
-values (322, 'SB3168');
+INSERT INTO votes (room_number, bill_name)
+VALUES (322, 'SB3168');
 
-insert into votes (room_number, bill_name)
-values (322, 'HB1082');
+INSERT INTO votes (room_number, bill_name)
+VALUES (322, 'HB1082');
 
-insert into votes (room_number, bill_name)
-values (322, 'SB5328');
+INSERT INTO votes (room_number, bill_name)
+VALUES (322, 'SB5328');
 
-insert into votes (room_number, bill_name)
-values (2442, 'SB5328');
+INSERT INTO votes (room_number, bill_name)
+VALUES (2442, 'SB5328');
 
-insert into votes (room_number, bill_name)
-values (2333, 'SB5328');
+INSERT INTO votes (room_number, bill_name)
+VALUES (2333, 'SB5328');
 
-insert into votes (room_number, bill_name)
-values (2333, 'SB5329');
+INSERT INTO votes (room_number, bill_name)
+VALUES (2333, 'SB5329');
 
-insert into votes (room_number, bill_name)
-values (106, 'SB5329');
+INSERT INTO votes (room_number, bill_name)
+VALUES (106, 'SB5329');
 
-insert into votes (room_number, bill_name)
-values (1233, 'SB5329');
+INSERT INTO votes (room_number, bill_name)
+VALUES (1233, 'SB5329');
 
-insert into votes (room_number, bill_name)
-values (1233, 'SB5168');
+INSERT INTO votes (room_number, bill_name)
+VALUES (1233, 'SB5168');
 
-insert into votes (room_number, bill_name)
-values (504, 'SB5168');
+INSERT INTO votes (room_number, bill_name)
+VALUES (504, 'SB5168');
 
-insert into votes (room_number, bill_name)
-values (2465, 'SB5168');
+INSERT INTO votes (room_number, bill_name)
+VALUES (2465, 'SB5168');
 
-insert into votes (room_number, bill_name)
-values (2465, 'SB5087');
+INSERT INTO votes (room_number, bill_name)
+VALUES (2465, 'SB5087');
 
-insert into votes (room_number, bill_name)
-values (1415, 'SB5087');
+INSERT INTO votes (room_number, bill_name)
+VALUES (1415, 'SB5087');
 
-insert into votes (room_number, bill_name)
-values (2417, 'SB5087');
+INSERT INTO votes (room_number, bill_name)
+VALUES (2417, 'SB5087');
 
-insert into votes (room_number, bill_name)
-values (2417, 'SB5016');
+INSERT INTO votes (room_number, bill_name)
+VALUES (2417, 'SB5016');
 
-insert into votes (room_number, bill_name)
-values (2417, 'SB5066');
+INSERT INTO votes (room_number, bill_name)
+VALUES (2417, 'SB5066');
 
-insert into votes (room_number, bill_name)
-values (2417, 'SB4978');
+INSERT INTO votes (room_number, bill_name)
+VALUES (2417, 'SB4978');
 
-insert into votes (room_number, bill_name)
-values (2332, 'SB4949');
+INSERT INTO votes (room_number, bill_name)
+VALUES (2332, 'SB4949');
 
-insert into votes (room_number, bill_name)
-values (2413, 'SB4949');
+INSERT INTO votes (room_number, bill_name)
+VALUES (2413, 'SB4949');
 
-insert into votes (room_number, bill_name)
-values (2413, 'SB4926');
+INSERT INTO votes (room_number, bill_name)
+VALUES (2413, 'SB4926');
 
-insert into votes (room_number, bill_name)
-values (2442, 'SB4926');
+INSERT INTO votes (room_number, bill_name)
+VALUES (2442, 'SB4926');
 
-insert into votes (room_number, bill_name)
-values (2188, 'SB4926');
+INSERT INTO votes (room_number, bill_name)
+VALUES (2188, 'SB4926');
 
-insert into votes (room_number, bill_name)
-values (2188, 'SB4439');
+INSERT INTO votes (room_number, bill_name)
+VALUES (2188, 'SB4439');
 
-insert into votes (room_number, bill_name)
-values (106, 'SB4439');
+INSERT INTO votes (room_number, bill_name)
+VALUES (106, 'SB4439');
 
-insert into votes (room_number, bill_name)
-values (106, 'SB4411');
+INSERT INTO votes (room_number, bill_name)
+VALUES (106, 'SB4411');
 
-insert into votes (room_number, bill_name)
-values (1226, 'SB4411');
+INSERT INTO votes (room_number, bill_name)
+VALUES (1226, 'SB4411');
 
-insert into votes (room_number, bill_name)
-values (1226, 'HB7939');
+INSERT INTO votes (room_number, bill_name)
+VALUES (1226, 'HB7939');
 
-insert into votes (room_number, bill_name)
-values (2413, 'HB7939');
+INSERT INTO votes (room_number, bill_name)
+VALUES (2413, 'HB7939');
 
-insert into votes (room_number, bill_name)
-values (2413, 'HB9701');
+INSERT INTO votes (room_number, bill_name)
+VALUES (2413, 'HB9701');
 

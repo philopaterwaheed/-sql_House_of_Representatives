c9a7fb09b31199c65c0d4b7687b9532e3a79ae5f
philo san <61416026+philopaterwaheed@users.noreply.github.com>
2023-12-10 06:58:03 +0200

    Update populate_cosponsors.sql

--- populate_cosponsors.sql
+++ populate_cosponsors.sql
@@ -30,3 +30,51 @@ values (20461, 'SB5312', 'Sen. Elizabeth Warren');
 
 insert into cosponsors (cosponsor_number, bill_name, cosponsor_name)
 values (204161, 'SB5311', 'Sen. Rob Portman');
+
+insert into cosponsors (cosponsor_number, bill_name, cosponsor_name)
+values (204151, 'HJR27', 'Tom Emmer');
+
+insert into cosponsors (cosponsor_number, bill_name, cosponsor_name)
+values (214151, 'HJR27', 'Jerry Carl');
+
+
+insert into cosponsors (cosponsor_number, bill_name, cosponsor_name)
+values (624091, 'HB3091', 'Ann Wagner');
+
+
+insert into cosponsors (cosponsor_number, bill_name, cosponsor_name)
+values (624211, 'HB3091', 'Brett Guthrie');
+
+
+insert into cosponsors (cosponsor_number, bill_name, cosponsor_name)
+values (622111, 'HB1118', 'Diana DeGette');
+
+
+insert into cosponsors (cosponsor_number, bill_name, cosponsor_name)
+values (621111, 'HB1118', 'Kevin Mullin');
+
+
+insert into cosponsors (cosponsor_number, bill_name, cosponsor_name)
+values (61111, 'HB5240', 'Jamie Raskin');
+
+
+insert into cosponsors (cosponsor_number, bill_name, cosponsor_name)
+values (60111, 'HB5240', 'Jahana Hayes');
+
+
+insert into cosponsors (cosponsor_number, bill_name, cosponsor_name)
+values (16911, 'HB431', 'James Comer');
+
+
+insert into cosponsors (cosponsor_number, bill_name, cosponsor_name)
+values (169121, 'HB431', 'Robert Good');
+
+
+insert into cosponsors (cosponsor_number, bill_name, cosponsor_name)
+values (169141, 'HB38', 'Austin Scott');
+
+
+insert into cosponsors (cosponsor_number, bill_name, cosponsor_name)
+values (169122, 'HB38', 'Tom Tiffany');
+
+
